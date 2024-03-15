d0a3ce2ad7730569e9fc2fe82ad05931fce33236
FernandoDeMeer <ferdemeer@gmail.com>
2024-03-15 15:36:16 +0100

    renaming + utils file with #TODOS

---
+++ data/OSDG/osdg_utils.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+import pandas as pd
+
+#TODO: Implement functions to load data from the OSDG dataset and return it as a pandas dataframe with the following columns:
+# - 'text' (the text of the sample)
+# - SDG (the corresponding SDG label)
+# - 'label' (the agreement value that we will use as the target variable)
+
+
+
+#TODO Implement a function to load the SDG goal/subgoal descriptions from the file 'data/OSDG/sdg_descriptions.csv' and return it as a pandas dataframe with the following columns:
+# - 'SDG' (the SDG label)
+# - 'description' (the description of the SDG goal/subgoal)
