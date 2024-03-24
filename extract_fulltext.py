862f360c5c4c1217335f1370210f858bc1f54296
veralara.bernhard <veralara.bernhard@uzh.ch>
2024-03-24 15:33:18 +0100

    Get correct pdfs

--- extract_fulltext.py
+++ extract_fulltext.py
@@ -6,7 +6,9 @@ import requests
 from bs4 import BeautifulSoup
 
 from helper import read_in_data
-
+from settings import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def get_pdf_from_zora_url(url: str, id: str) -> str:
     '''Get the html from a url.'''
@@ -14,22 +16,87 @@ def get_pdf_from_zora_url(url: str, id: str) -> str:
         response = requests.get(url)
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
-        # find the link in <a> with class ep_document_link
-        pdf_link = soup.find('a', class_='ep_document_link')
-        if pdf_link:
-            pdf_url = pdf_link.get('href')
-            # download the pdf
-            response = requests.get(pdf_url)
-            with open(f'data/pdf/{id}.pdf', 'wb') as file:
-                file.write(response.content)
-        else:
-            # write the url to a file
-            with open('data/pdf/missing_pdfs.txt', 'a') as file:
-                file.write(f'{url}\n')
-    
+        # find <span class="download_div_oa_text">Closed Access</span>
+        access = soup.find('span', class_='download_div_oa_text').text
+        downloadable = ['Hybrid Open Access',
+                        'Gold Open Access', 'Green Open Access']
+
+        if access == 'Closed Access':
+            no_pdf = soup.find('div', class_='download_meta').text
+            if no_pdf == 'Full text not available from this repository.':
+                # write the url to a file
+                with open('data/pdf/missing_pdfs.txt', 'a') as file:
+                    file.write(f'{url}\n')
+            else:
+                # write the url to a file
+                with open('data/pdf/closed_pdfs.txt', 'a') as file:
+                    file.write(f'{url}\n')
+
+        elif access in downloadable:
+            # find the link in <a> with class ep_document_link
+            pdf_link = soup.find('a', class_='ep_document_link')
+            if pdf_link:
+                id = url.lstrip('oai:www.zora.uzh.ch:')
+                pdf_url = pdf_link.get('href')
+                # download the pdf
+                get_pdf(pdf_url, id)
+                
+
     except requests.exceptions.RequestException as e:
         print(e)
         return None
+
+def get_pdf(pdf_url: str, id: str, ext='zora'):
+    response = requests.get(pdf_url)
+    with open(f'data/pdf/{id}_{ext}.pdf', 'wb') as file:
+        file.write(response.content)
+
+def get_closed_pdf_from_zora_url(file: str) -> str:
+    """ Little bot to open zora, login, prompt sso code, and download the pdfs."""
+    # Login to zora
+    driver = webdriver.Chrome()
+    driver.get('https://www.zora.uzh.ch/cgi/users/home')
+    driver.implicitly_wait(5)
+    username = driver.find_element(By.ID, 'username')
+    username.send_keys(EMAIL)
+    login = driver.find_element(By.ID, 'login-button')
+    login.click()
+    driver.implicitly_wait(5)
+    password = driver.find_element(By.ID,'password')
+    password.send_keys(PASSWORD)
+    login = driver.find_element(By.ID, 'login-button')
+    login.click()
+    driver.implicitly_wait(5)
+
+    # Prompt the user for two factor authentication code
+    otp = driver.find_element(By.ID, 'otp')
+    # if opt exists
+    if otp:
+        # prompt the user to enter the otp
+        code = input('Enter the code: ')
+        otp.send_keys(code)
+        # press login
+        # <button type="submit" id="login-button" name="_eventId_submit" class="btn btn-primary btn-lg btn-block" style="margin-bottom: 15px;">Login</button>
+        login = driver.find_element(By.ID, 'login-button')
+        login.click()
+        # wait for the page to load
+        driver.implicitly_wait(5)
+
+    # fetch all closed pdfs
+    with open(file, 'r') as file:
+        urls = file.readlines()
+        for url in urls:
+            url = url.strip()
+            driver.get(url)
+            # <a href="https://www.zora.uzh.ch/id/eprint/132070/1/1-s2.0-S0093691X16000686-main.pdf" class="download_logged_in btn btn-uzh-prime" title="Download PDF ">Download
+            # <span class="ep_document_citation">PDF</span>
+            pdf_link = driver.find_element(By.CLASS_NAME, 'download_logged_in').get_attribute('href')
+            if pdf_link:
+                id = url.split('/')[-1]
+                # download the pdf
+                get_pdf(pdf_link, id, 'closed')
+            else:
+                print(f'No pdf found for {url}')
 
 def extract_text_from_pdf(pdf: str) -> str:
     text = ""
@@ -55,10 +122,11 @@ def text_mine_pdfs(pdf_folder: str) -> None:
                 # write the pdfs and error to a file
                 with open('data/pdf/faulty_pdf_text_extract.txt', 'a') as file:
                     file.write(f'{pdf}: {e}\n')
-            
+
     # write all text to a json file
     with open('data/pdf/pdf_text_raw.json', 'w', encoding='utf-8') as file:
         json.dump(pdf_text_dict, file, ensure_ascii=False, indent=4)
+
 
 if __name__ == '__main__':
     # read in the data
@@ -68,4 +136,11 @@ if __name__ == '__main__':
         id = row['ID'].lstrip('oai:www.zora.uzh.ch:')
         get_pdf_from_zora_url(row['URL'], id)
 
+    # get the closed pdfs
+    closed_pdf = 'data/pdf/closed_pdfs.txt'
+    get_closed_pdf_from_zora_url(closed_pdf)
+    
     # text_mine_pdfs('data/pdf')
+    # url = 'https://www.zora.uzh.ch/id/eprint/149430/'
+    # id = url.lstrip('oai:www.zora.uzh.ch:')
+
