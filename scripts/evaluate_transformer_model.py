4a20d88f9d30a35c4ff1fe78359d7c25c9ceb596
FernandoDeMeer <ferdemeer@gmail.com>
2024-04-23 11:43:30 +0000

    evaluate transformer model script + testing bugfixing

--- scripts/evaluate_transformer_model.py
+++ scripts/evaluate_transformer_model.py
@@ -3,42 +3,69 @@ import sys
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 sys.path.append(os.getcwd())
-from models.mbert.pytorch_model import PyTorchModel
-from models.mbert.config import read_arguments_eval
+from src.models.mbert.config import read_arguments_eval
 from src.helpers.logging_helper import setup_logging
 from src.helpers.path_helper import experiment_file_path
-from scripts.analyze_prediction_log import calculate_scores, plot_scores, save_label_scores, save_accuracy_score, plot_confusion_matrix
+from scripts.analyze_prediction_log import calculate_scores, save_label_scores, plot_confusion_matrix
 
 
 setup_logging()
 
 def main(args):
     # Load the prediction log of the model
-    prediction_log_path = 'models/{}'.format(args.experiment_name) + '/mbert__prediction_log_epoch{}.csv'.format(args.epoch)
+    prediction_log_path = 'models/{}'.format(args.experiment_name) + '/mbert__prediction_log__ep{}.csv'.format(args.epoch)
     model_predictions = pd.read_csv(prediction_log_path)
-    model_scores = calculate_scores(model_predictions)
+    model_acc, model_scores = calculate_scores(model_predictions)
 
     # Benchmark predictions
 
-    benchmark_prediction_log_path = 'models/{}'.format('mbert_seed_0_swisstext_task1_train') + '/mbert__benchmark_prediction_log_epoch{}.csv'.format(args.epoch)
+    benchmark_prediction_log_path = 'models/{}'.format('mbert_seed_0_swisstext_task1_train') + '/mbert__prediction_log__ep{}.csv'.format(args.epoch)
     benchmark_predictions = pd.read_csv(benchmark_prediction_log_path)
-    benchmark_scores = calculate_scores(benchmark_predictions)
+    benchmark_acc, benchmark_scores = calculate_scores(benchmark_predictions)
+
+    # Print the accuracies and save the F1 scores for each label
+
+    print('Model accuracy: {}'.format(model_acc))
+    print('Benchmark accuracy: {}'.format(benchmark_acc))
+    save_label_scores(model_scores, args.experiment_name, args.epoch)
+    save_label_scores(benchmark_scores, args.experiment_name, args.epoch, benchmark=True)
 
     # Plot the scores together
-
+    plot_scores_vs_benchmark(model_scores, benchmark_scores, args.experiment_name, args.epoch)
 
 
 def plot_scores_vs_benchmark(model_scores, benchmark_scores, experiment_name, epoch):
-    plt.figure(figsize=(10, 5))
-    plt.bar(model_scores.keys(), [score['f1'] for score in model_scores.values()], label='{}'.format(experiment_name))
-    plt.bar(benchmark_scores.keys(), [score['f1'] for score in benchmark_scores.values()], label='Benchmark')
-    plt.xlabel('SDG')
-    plt.ylabel('F1 Score')
-    plt.title(f'F1 Scores for {experiment_name} at epoch {epoch} vs Benchmark Model')
-    plt.legend()
-    plt.savefig(experiment_file_path(experiment_name, "".join(['mbert', '__f1_scores__ep', str(epoch), '.png'])))
+    # Plot the F1 scores in a Fig with 2 subplots
+    fig, axs = plt.subplots(2, 1, figsize=(20, 10))
+
+    # Plot the F1 scores of the model
+    model_scores = pd.DataFrame(model_scores)
+    model_scores = model_scores.transpose()
+    # Sort the labels by the SDG (i.e. 0 to 17)
+    model_scores = model_scores.sort_index()
+    model_scores.plot(kind='bar', ax=axs[0])
+    axs[0].set_title('{} F1 scores'.format(experiment_name))
+    axs[0].set_xlabel('Label')
+    axs[0].set_ylabel('F1 score')
+
+    # Plot the F1 scores of the benchmark
+    benchmark_scores = pd.DataFrame(benchmark_scores)
+    benchmark_scores = benchmark_scores.transpose()
+    # Sort the labels by the SDG (i.e. 0 to 17)
+    benchmark_scores = benchmark_scores.sort_index()
+    benchmark_scores.plot(kind='bar', ax=axs[1])
+    axs[1].set_title('Benchmark F1 scores')
+    axs[1].set_xlabel('Label')
+    axs[1].set_ylabel('F1 score')
+
+    # Save the plot
+    plot_path = experiment_file_path(experiment_name, 'mbert__scores_comparison__ep{}.png'.format(epoch))
+    plt.savefig(plot_path)
+
 
 if __name__ == '__main__':
     args = read_arguments_eval()
+    main(args)
--- src/models/mbert/run_transformer_testing.py
+++ src/models/mbert/run_transformer_testing.py
@@ -1,14 +1,15 @@
 import os
 import sys
+import copy
 
 sys.path.append(os.getcwd())
-from models.mbert.pytorch_model import PyTorchModel
+from src.models.mbert.pytorch_model import PyTorchModel
 from src.data.dataset import PytorchDataset
 from src.data.tokenizer import SwissTextTokenizer
 from torch.utils.data import Dataset, DataLoader
 
 from src.helpers.seed_helper import initialize_gpu_seed
-from models.mbert.config import read_arguments_test
+from src.models.mbert.config import read_arguments_test, update_args_with_config
 from src.helpers.logging_helper import setup_logging
 from src.helpers.path_helper import *
 
@@ -28,12 +29,44 @@ def main(args):
 
     model = PyTorchModel.load_from_checkpoint(args, checkpoint_path)
     
-    # We always want to compare models to mbert trained only on the train samples of the swisstext_task1_train dataset
+    # We always want to compare models to mbert trained only on the train samples of the swisstext_task1_train dataset trained with the following launch.json: 
+
+#     {
+#     // Use IntelliSense to learn about possible attributes.
+#     // Hover to view descriptions of existing attributes.
+#     // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+#     "version": "0.2.0",
+#     "configurations": [
+#         {
+#             "name": "Python Debugger: osdg_utils",
+#             "type": "debugpy",
+#             "request": "launch",
+#             "program": "src/models/mbert/run_transformer_training.py",
+#             "console": "integratedTerminal",
+#             "justMyCode": true,
+#             "env": {
+#             "PYTHONPATH": "anaconda3/envs/swisstextenv/bin/python",
+#             },
+#             "cwd": "${workspaceFolder}",
+#             "args": ["--experiment_name", "mbert_seed_0_swisstext_task1_train",
+#                      "--model_name", "mbert",
+#                      "--dataset", "swisstext_task1_train",
+#                      "--train_frac", "0.33",
+#                      "--save_model",
+#                      "--save_config",
+#                 ],
+#         }
+#     ]
+# }
+
   
     # Modify the args to load the swisstext_task1_train dataset
-    args.experiment_name = 'mbert_seed_0_swisstext_task1_train'
-    args.epoch = 5
-    swisstext_model = PyTorchModel(args)
+    new_args = copy.deepcopy(args)
+    new_args.experiment_name = 'mbert_seed_0_swisstext_task1_train'
+    new_args.epoch = 5
+    new_args = update_args_with_config(experiment_name = new_args.experiment_name, args = new_args)
+
+    swisstext_model = PyTorchModel(new_args)
 
     # Now set the test data loader of our model to that of the swisstext_model
     model.test_data_loader = swisstext_model.test_data_loader
