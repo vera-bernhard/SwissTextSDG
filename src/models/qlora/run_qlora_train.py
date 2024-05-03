91bd9a6628cf960e13eb8c8fa2e96a3522e95ad2
vera-bernhard <veralara.bernhard@uzh.ch>
2024-05-03 11:10:24 +0000

    Add train command

--- src/models/qlora/run_qlora_train.py
+++ src/models/qlora/run_qlora_train.py
@@ -7,6 +7,10 @@ from src.helpers.seed_helper import initialize_gpu_seed
 from src.models.mbert.config import read_arguments_train
 from src.helpers.logging_helper import setup_logging
 
+# python src/models/qlora/run_qlora_train.py --experiment_name qlora-mistral_swisstext_task1_train --model_name qlora-mistral --dataset swisstext_task1_train --save_model --save_config --train_frac 0.33 --batch_size 1 --num_epochs 5 --no_stopword_removal
+
+# python src/models/qlora/run_qlora_train.py --experiment_name qlora-mistral_swisstext_task1_train_big --model_name qlora-mistral --dataset combined_OSDG_swisstext_enlarged_OSDG_enlarged_swisstext --save_model --save_config --train_frac 1 --batch_size 1 --num_epochs 5 --no_stopword_removal
+
 
 setup_logging()
 
